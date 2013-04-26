4337d6fde10c34c3ee2e797464d86fa1defe9b3b
Guillaume <guillaume.gouchon@gmail.com>
2013-04-26 14:13:16 +0100

    u

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('changeGameData', function(data) {
+    console.log(data);
     app.gamesManager.changeGameData(data);
   });
 
