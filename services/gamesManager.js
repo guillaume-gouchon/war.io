d8265c691ac6de4c01773ab50aaa3ba7ca0b0370
Guillaume <guillaume.gouchon@gmail.com>
2013-04-26 14:54:29 +0100

    update

--- services/gamesManager.js
+++ services/gamesManager.js
@@ -259,7 +259,7 @@ module.exports = function(app){
       			//change number of players
       			if (game.players.length <= gameData.nbPlayers) {
 	      			game.nbPlayers = gameData.nbPlayers;
-	      			if (game.players.length == gamData.nbPlayers) {
+	      			if (game.players.length == gameData.nbPlayers) {
 						app.gamesManager.startGame(game);
 	      			}
       			}
