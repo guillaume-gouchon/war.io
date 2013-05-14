b1f5e4e4ea7a52b9e790663ce0073b3ac3fc5e7e
Guillaume <guillaume.gouchon@gmail.com>
2013-05-14 11:26:12 +0100

    update

--- public/js/game/client/gameManager.js
+++ public/js/game/client/gameManager.js
@@ -240,7 +240,7 @@ gameManager.updatePlayersInGame = function (data) {
 
 	$('#igPlayersList').removeClass('hide').html('');
 	for (var i in data.players) {
-		$('#igPlayersList').append('<div class="' + gameSurface.PLAYERS_COLORS[i] +  ' ' + (data.players[i].ready == 1 ? 'ready':'') + '" data-id="' + data.players[i].pid + '">' + data.players[i].n + '</div>');
+		$('#igPlayersList').append('<div class="' + gameSurface.PLAYERS_COLORS[i] + '" data-id="' + data.players[i].pid + '">' + data.players[i].n + (gameContent.myArmy == null || data.players[i].ready == 1 ? '':' (Loading Game...)') + '</div>');
 	}
 }
 
