b85a557d4ddffa167bee58d44cc36ab19139ab7c
Guillaume <guillaume@warnode.(none)>
2014-04-28 08:19:52 -0400

    u

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ grunt build;
 # Restart server
 cd ..;
 sudo forever stop warnode;
-forever --uuid warnode -a -l node.log -o node.log -e node.log start warnode.js;
+forever --uid "warnode" -a -l node.log -o node.log -e node.log start warnode.js;
